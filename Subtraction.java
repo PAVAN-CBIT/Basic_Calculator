eeb75c618f70b8b3f3ece6f355c751729bf4c31c
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-09-02 15:39:55 +0530

    Create Subtraction.java

---
+++ Subtraction.java
@@ -0,0 +1,10 @@
+public class Subtraction
+{
+static void subtraction(int a,int b)
+    {
+        
+        int c;
+        c=b-a;
+         System.out.println("Subtraction :-"+c);
+        
+}    }

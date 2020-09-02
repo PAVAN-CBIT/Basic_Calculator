f854e8a4aa836bd7e784dd276f08215d2bc49c91
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-09-02 15:41:10 +0530

    Create Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+public class Multiplication
+{
+static void multiplication(int a,int b)
+    {
+        int c;
+        c=a*b;
+         System.out.println("Multiplication :-"+c);
+        
+        
+        
+    }
+}    
